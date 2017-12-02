57eba922ed35a453e70a597482ae11eb475da09c
raypex <raypex2@gmail.com>
2017-12-02 14:59:59 +0100

    A little more done to the Xml

--- app/src/main/java/me/rayokaah/mlh/MainActivity.java
+++ app/src/main/java/me/rayokaah/mlh/MainActivity.java
@@ -1,7 +1,8 @@
 package me.rayokaah.mlh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void requestPickup() {
+        EditText description = (EditText) findViewById(R.id.description);
+        String descriptionText = description.getText().toString();
+        EditText name = (EditText) findViewById(R.id.name_field);
+        String nameText = name.getText().toString();
+        /** To do write the code to store in the database
+         * send descriptionText and nameTesxt and send notification with the info to the Gabbage collector
+         */
+
     }
 
 }
